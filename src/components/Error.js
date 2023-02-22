1aca7b84645d779894192a092269ed57c693967c
Tejas <tejasvispute25@gmail.com>
2023-02-22 13:15:47 +0530

    final touch added frontend

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import "../componentcss/error.css";
 const Error = () => {
   return (
     <>
-      <h1 className="error-header">404 Not Found</h1>
+      <h1 className="error-header">404 Page Not Found </h1>
       <button className="btn btn-danger">
         <Link to="/" style={{ textDecoration: "none", color: "white" }}>
           Go to Home
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,7 +27,7 @@ function Home() {
             />
             <img
               className="mt-2"
-              src="https://cdn.testbook.com/products/2022/July/26/__w-200-400-600__/Full-Stack-Development-Career-Program-(6-Months)_Course-Card.jpg.jpeg"
+              src="https://cdn.testbook.com/products/2022/July/14/__w-200-400-600__/image%20%281%29.png.png"
               alt=""
             />
           </div>
